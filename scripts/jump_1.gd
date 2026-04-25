2f975603c0aa37e0746d8126849ba9cfbd954311
Nekaque <3217cody@gmail.com>
2026-04-25 03:23:17 +0200

    parallax fixed

--- scripts/jump_1.gd
+++ scripts/jump_1.gd
@@ -3,15 +3,13 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	#GM.from_cave()
 	pass
-
+	#scroll_offset = $Jumper/Camera2D.get_screen_center_position() * (Vector2.ONE - scroll_scale)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
+func _on_end_body_entered(body: Node2D) -> void: GM.from_cave()
 
-func _on_end_body_entered(body: Node2D) -> void:
-	print(body.name)
-	GM.from_cave()
+func _on_death_body_entered(body: Node2D) -> void: GM.death()
