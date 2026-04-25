0046b30598fec563d8a1e5f6019e74262eec4d73
Nekaque <3217cody@gmail.com>
2026-04-25 18:42:39 +0200

    full pipeline with reset

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -4,7 +4,7 @@ var controllable = true
 @onready var map = $Canvas/Map
 @onready var path_follower = $Canvas/Map/Path1/PathFollow2D
 const fade_time = 0.5
-var current = 0
+var current_index = 0
 
 var jumps = ["res://scenes/jump/jump1.tscn"]
 var throws = ["res://scenes/throw/throw1.tscn"]
@@ -23,18 +23,22 @@ func trigger_shake(strength: float = 15.0, decay: float = 0.5) -> void:
 	fading = decay
 	
 func to_tutorial():
+	current_index = 0
+	path_follower.reparent(paths[current_index])
 	controllable = false
 	fade.texture = cave
 	var tw = get_tree().create_tween()
 	tw.tween_property(fade, "modulate:a",1,fade_time)
 	await tw.finished
-	current_scene = jumps[current]
+	current_scene = jumps[current_index]
 	get_tree().change_scene_to_file(tut)
 	fade_out()
 
 func to_epilogue():
-	get_tree().change_scene_to_file("res://scenes/epilogue/epilogue.tscn")
-	
+	get_tree().change_scene_to_file("res://scenes/epilogue.tscn")
+
+func to_prologue():
+	get_tree().change_scene_to_file("res://scenes/prologue.tscn")
 	
 
 func to_cave():
@@ -43,7 +47,7 @@ func to_cave():
 	var tw = get_tree().create_tween()
 	tw.tween_property(fade, "modulate:a",1,fade_time)
 	await tw.finished
-	current_scene = jumps[current]
+	current_scene = jumps[current_index]
 	get_tree().change_scene_to_file(current_scene)
 	fade_out()
 
@@ -53,7 +57,7 @@ func from_cave():
 	var tw = get_tree().create_tween()
 	tw.tween_property(fade, "modulate:a",1,fade_time)
 	await tw.finished
-	current_scene = throws[current]
+	current_scene = throws[current_index]
 	get_tree().change_scene_to_file(current_scene)
 	fade_out()
 
@@ -83,7 +87,7 @@ func to_map():
 	tw = get_tree().create_tween()
 	tw.tween_property(map, "modulate:a",0,fade_time)
 	await tw.finished
-	path_follower.reparent(paths[1-current])
+	path_follower.reparent(paths[1-current_index])
 	path_follower.progress_ratio = 0
 	controllable = true
 	#fade_out()
