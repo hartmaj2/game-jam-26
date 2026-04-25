21be5fe6437f8f94ecfef2c67e02b224cab0819f
Nekaque <3217cody@gmail.com>
2026-04-25 11:32:48 +0200

    insane transitions

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -8,6 +8,11 @@ var jumps = ["res://scenes/jump1.tscn"]
 var throws = ["res://scenes/throw1.tscn"]
 var current_scene = jumps[0]
 
+var death_screen = preload("res://assets/img/death.jpg")
+var village = preload("res://assets/img/village.jpg")
+var cave = preload("res://assets/img/cave.jpg")
+var map = preload("res://assets/img/map.jpg")
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -19,9 +24,9 @@ func _process(delta: float) -> void:
 
 func to_cave():
 	controllable = false
-	fade.color = Color(0,0,0,0)
+	fade.texture = cave
 	var tw = get_tree().create_tween()
-	tw.tween_property(fade, "color:a",1,fade_time)
+	tw.tween_property(fade, "modulate:a",1,fade_time)
 	await tw.finished
 	current_scene = jumps[current]
 	get_tree().change_scene_to_file(current_scene)
@@ -29,17 +34,17 @@ func to_cave():
 
 func to_map():
 	controllable = false
-	fade.color = Color(0,0,0,0)
+	fade.texture = map
 	var tw = get_tree().create_tween()
-	tw.tween_property(fade, "color:a",1,fade_time)
+	tw.tween_property(fade, "modulate:a",1,fade_time)
 	await tw.finished
 	fade_out()
 
 func from_cave():
 	controllable = false
-	fade.color = Color(1,1,1,0)
+	fade.texture = village
 	var tw = get_tree().create_tween()
-	tw.tween_property(fade, "color:a",1,fade_time)
+	tw.tween_property(fade, "modulate:a",1,fade_time)
 	await tw.finished
 	current_scene = throws[current]
 	get_tree().change_scene_to_file(current_scene)
@@ -47,9 +52,9 @@ func from_cave():
 
 func death():
 	controllable = false
-	fade.color = Color(1,0,0,0)
+	fade.texture = death_screen
 	var tw = get_tree().create_tween()
-	tw.tween_property(fade, "color:a",1,fade_time)
+	tw.tween_property(fade, "modulate:a",1,fade_time)
 	await tw.finished
 	get_tree().change_scene_to_file(current_scene)
 	fade_out()
@@ -57,4 +62,4 @@ func death():
 func fade_out():
 	controllable = true
 	var tw = get_tree().create_tween()
-	tw.tween_property(fade, "color:a",0,fade_time)
+	tw.tween_property(fade, "modulate:a",0,fade_time)
