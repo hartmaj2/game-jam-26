8ca60d17a35388be6be8c13625646ff0c38cfa62
Nekaque <3217cody@gmail.com>
2026-04-25 05:03:38 +0200

    sync

--- scripts/jump_1.gd
+++ scripts/jump_1.gd
@@ -4,7 +4,6 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
-	#scroll_offset = $Jumper/Camera2D.get_screen_center_position() * (Vector2.ONE - scroll_scale)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
